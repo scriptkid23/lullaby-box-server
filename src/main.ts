272ea8d835d754a1fb56a8c83880ad945dae4b25
HoanDo <sieunhannhenkutesn@gmail.com>
2021-06-20 19:52:17 +0700

    feat: init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Metaphor')
+    .setDescription("let's talk")
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document);
+
   const config = app.get(ConfigService);
   const port = process.env.PORT || 3000;
   await app.listen(port, () => {
