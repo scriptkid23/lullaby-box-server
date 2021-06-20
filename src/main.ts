1d10d7fc76bcf39659633c4cf3dd363f40996aaa
HoanDo <sieunhannhenkutesn@gmail.com>
2021-06-20 11:04:50 +0700

    update init project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    Logger.log('Listening at http://localhost:' + port);
+  });
+  Logger.log('Configuration port:' + config.get('port'));
 }
 bootstrap();
