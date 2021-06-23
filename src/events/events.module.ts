64d6021ea4167c7c80c91bbda5ded78a4a7ff75c
HoanDo <sieunhannhenkutesn@gmail.com>
2021-06-23 16:08:11 +0700

    feat: init audio gateway

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AudioGateway } from './audio.gateway';
 import { ChatGateway } from './chat.gateway';
 import { EventsGateway } from './events.gateway';
 
 @Module({
-  providers: [EventsGateway, ChatGateway],
+  providers: [EventsGateway, ChatGateway, AudioGateway],
 })
 export class EventsModule {}
